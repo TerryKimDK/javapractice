877590bebc9045a1cd07783efe255ea7f1be2667
TerryKimDK <kimdongkyuterry@gmail.com>
2022-09-05 17:46:13 +0900

    Dk

---
+++ AutoBank/src/application/Bank.java
@@ -0,0 +1,10 @@
+package application;
+
+public interface Bank {
+	
+	//boolean deposit(Ex2 cust, long input);
+	boolean deposit();
+	boolean withDraw();
+	boolean createAccount();
+	boolean accountList();
+}
---
+++ AutoBank/src/application/Client.java
@@ -0,0 +1,9 @@
+//package application;
+//
+//public class Client {
+//	String name;
+//	int age;
+//	int cash;
+//	BankAccount account;
+//	
+//}
